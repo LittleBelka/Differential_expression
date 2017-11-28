d79722ca76a70370f18d094ffb9043a7fe801986
Ekaterina Belyaeva <Kompp@yandex.ru>
2017-11-28 05:06:49 +0300

    feat: delet import GEOquery library from start file

--- dif_expression/start_diff_expression.R
+++ dif_expression/start_diff_expression.R
@@ -1,7 +1,5 @@
-library(GEOquery)
 # for differentialExpression
 source("./dif_expression/couple_universal.R")
-
 options(download.file.method.GEOquery = "libcurl")
 
 dataSetSeries <- c("GSE53986", "GSE50122", "GSE61055", "GSE14308")
