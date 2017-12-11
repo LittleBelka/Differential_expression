3f3e07833e67f59e76e2afa51c71cde6ead3b58e
Ekaterina Belyaeva <Kompp@yandex.ru>
2017-12-12 01:57:42 +0300

    delete: some useless scripts

--- dif_expression/start_diff_expression.R
+++ dif_expression/start_diff_expression.R
@@ -5,8 +5,8 @@ options(download.file.method.GEOquery = "libcurl")
 
 # dataSetSeries <- c("GSE53986", "GSE50122", "GSE61055", "GSE14308")
 #dataSetSeries <- c("GSE38230-GPL10123")
-# dataSetSeries <- c("GSE61055")
-dataSetSeries <- c("GSE9943")
+dataSetSeries <- c("GSE61055")
+# dataSetSeries <- c("GSE35724")
 fileWithGenes <- "di.dn200.sig.txt"
 
 # View(head(fData(a_es00)))
